08985758f21379e13929dc1369f1b87ffc9060f2
Don Park <don@donpark.org>
2012-09-20 16:09:46 -0700

    time series graph

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,12 +6,15 @@ function setup(d1, d2) {
   var options = {
     series: {
         lines: { show: true },
-        points: { show: true }
-    }
+        points: { show: true },
+    },
+    xaxis: { mode: 'time',
+             timeformat: "%m/%d %M:%H",
+             tickSize: [1, "hour"]}
   };
 
   $.plot($("#chart"), [
-      { label: "d1",  data: [d1]},
-      { label: "d2",  data: [d2]}
+      { label: "d1",  data: d1},
+      { label: "d2",  data: d2}
   ], options);
 }
