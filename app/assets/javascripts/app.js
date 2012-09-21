3bb7e97728092b51fa91c391039ddc77c58ec009
Don Park <don@donpark.org>
2012-09-21 10:06:11 -0700

    chart the spread

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -12,12 +12,27 @@ function setup(data) {
   };
 
   var dset = []
-  for(var datum in data) {
-    dset.push({label: data[datum][0]+" ask",
-               data: data[datum][2]})
-    dset.push({label: data[datum][0]+" bid",
-               data: data[datum][1]})
+  for(var market_id in data) {
+    dset.push({label: data[market_id][0]+" ask",
+               data: data[market_id][2]})
+    dset.push({label: data[market_id][0]+" bid",
+               data: data[market_id][1]})
   }
+
+  var plusdiffs = []
+  var minusdiffs = []
+  for(var mdata in data[0][2]) {
+    var point = data[0][2][mdata]
+    var other_point = data[1][2][mdata]
+    var diff = point[1]-other_point[1]
+    if(diff > 0) {
+      plusdiffs.push([point[0], diff])
+    } else {
+      minusdiffs.push([point[0], diff])
+    }
+  }
+  dset.push({label: 'mtgox yes', data: plusdiffs, yaxis: 2})
+  dset.push({label: 'bitstamp yes', data: minusdiffs, yaxis: 2})
 
   console.log(dset)
   $.plot($("#chart"), dset, options);
