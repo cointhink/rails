8d8dabdbf5b5218d9a9c96b0523a2041b8edcb0f
Don Park <don@donpark.org>
2012-09-20 22:18:48 -0700

    market balances

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,4 +1,7 @@
 class Balance < ActiveRecord::Base
   belongs_to :market
   attr_accessible :amount, :currency
+
+  scope :usd, where(:currency => "usd")
+  scope :btc, where(:currency => "btc")
 end
--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,15 @@
 class Market < ActiveRecord::Base
   attr_accessible :name
   has_many :tickers
+  has_many :balances
+
+  def usd_balance
+    last = balances.usd.last
+    last ? last.amount : 0
+  end
+
+  def btc_balance
+    last = balances.btc.last
+    last ? last.amount : 0
+  end
 end
