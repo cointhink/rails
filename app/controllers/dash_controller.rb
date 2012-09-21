a539ba609a87904ab02255549be553cce2c02e08
Don Park <don@donpark.org>
2012-09-21 09:38:44 -0700

    one day of history

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -1,6 +1,6 @@
 class DashController < ApplicationController
   def chart
-    @data = Market.all.map do |market|
+    @data = Market.where("created_at > ?", 1.day.ago).map do |market|
       [ market.name,
         market.tickers.map{|t| [t.created_at.to_i*1000, t.highest_bid_usd.to_f]},
         market.tickers.map{|t| [t.created_at.to_i*1000, t.lowest_ask_usd.to_f]}]
