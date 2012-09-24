67c96218fbfa9ff45cbb61f8bcfb7661a6871260
Don Park <don@donpark.org>
2012-09-24 13:12:53 -0700

    plusminus diffs not right

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -21,21 +21,6 @@ function setup(data) {
                data: data[market_id][1]})
   }
 
-  var plusdiffs = []
-  var minusdiffs = []
-  for(var mdata in data[0][2]) {
-    var point = data[0][2][mdata]
-    var other_point = data[1][2][mdata]
-    var diff = point[1]-other_point[1]
-    if(diff > 0) {
-      plusdiffs.push([other_point[0], diff])
-    } else {
-      minusdiffs.push([point[0], diff])
-    }
-  }
-  dset.push({label: 'bitstamp buy', data: plusdiffs, yaxis: 2, points: {show: false}})
-  dset.push({label: 'mtgox buy', data: minusdiffs, yaxis: 2, points: {show: false}})
-
   console.log(dset)
   $.plot($("#chart"), dset, options);
 }
