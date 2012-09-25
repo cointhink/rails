68c59ce3b89b6d8cba30a3f061da5b51fce2cee9
Don Park <don@donpark.org>
2012-09-25 09:46:33 -0700

    btc-e api bug

--- app/models/markets/btce.rb
+++ app/models/markets/btce.rb
@@ -1,7 +1,8 @@
 class Markets::Btce
   def data_poll
     data = JSON.parse(Faraday.get('https://btc-e.com/api/2/btc_usd/ticker').body)["ticker"]
-    attrs = {:highest_bid_usd => data["buy"],
-             :lowest_ask_usd => data["sell"]}
+    #unbelivable bug - buy/sell is swapped in the api results
+    attrs = {:highest_bid_usd => data["sell"],
+             :lowest_ask_usd => data["buy"]}
   end
 end
