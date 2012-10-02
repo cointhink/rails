766f613f893dde5e482dbeec4353082fdb999057
Don Park <don@donpark.org>
2012-10-02 15:12:02 -0700

    6 hours ago

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -1,12 +1,13 @@
 class DashController < ApplicationController
   def chart
+    time = 6.hours.ago
     @data = Market.all.map do |market|
       [ market.exchange.name,
-        market.tickers.where("created_at > ?", 10.hours.ago).map{|t| [t.created_at.to_i*1000, t.highest_bid_usd.to_f]},
-        market.tickers.where("created_at > ?", 10.hours.ago).map{|t| [t.created_at.to_i*1000, t.lowest_ask_usd.to_f]}]
+        market.tickers.where("created_at > ?", time).map{|t| [t.created_at.to_i*1000, t.highest_bid_usd.to_f]},
+        market.tickers.where("created_at > ?", time).map{|t| [t.created_at.to_i*1000, t.lowest_ask_usd.to_f]}]
     end
 
-    @strategies = Strategy.where(["created_at > ?", 10.hours.ago]).map{|s| [s.created_at.to_i*1000, s.potential.amount.to_f]}
+    @strategies = Strategy.where(["created_at > ?", time]).map{|s| [s.created_at.to_i*1000, s.potential.amount.to_f]}
     @strategy = Strategy.last
   end
 end
