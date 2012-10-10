74843e3fa70507bc68a6fcb817ce58b765cfb9e4
Don Park <don@donpark.org>
2012-10-10 13:34:34 -0700

    Balance test

--- test/unit/balance_test.rb
+++ test/unit/balance_test.rb
@@ -1,6 +1,29 @@
 require 'test_helper'
 
 class BalanceTest < ActiveSupport::TestCase
-  test "creation" do
+  test "make_usd" do
+    balance = Balance.make_usd(10)
+    assert_equal 10, balance.amount
+    assert_equal 'usd', balance.currency
+  end
+
+  test "make_btc" do
+    balance = Balance.make_btc(8)
+    assert_equal 8, balance.amount
+    assert_equal 'btc', balance.currency
+  end
+
+  test "multiplication" do
+    balance = Balance.make_btc(8)
+    answer = balance * 2
+    assert_equal 16, answer.amount
+    assert_equal 'btc', answer.currency
+  end
+
+  test "division" do
+    balance = Balance.make_btc(8)
+    answer = balance / 2
+    assert_equal 4, answer.amount
+    assert_equal 'btc', answer.currency
   end
 end
