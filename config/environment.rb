dfe23a6efba938d66d17105daa33a2eb0656d12b
Don Park <don@donpark.org>
2012-10-15 10:03:23 -0700

    i like pretty html, even in production

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Slim::Engine.set_default_options(pretty: true)
+
 # Initialize the rails application
 Btcmarketwatch::Application.initialize!
