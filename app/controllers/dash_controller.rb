9f20bf256e8d01203637b20a48320c776b3e1789
Don Park <don@donpark.org>
2012-11-07 14:05:32 -0800

    display data using new Snapshot model

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -1,28 +1,42 @@
 class DashController < ApplicationController
   def chart
-    time = 8.hours.ago
-    exchanges = Market.internal.trading('btc','usd').map{|m| m.exchange}
+    cutoff = 8.hours.ago
+    snapshots = Snapshot.where('created_at > ?', cutoff).order('created_at desc')
+    snapshot = snapshots.last
+    exchanges = snapshot.exchanges
 
-    @chart_data = exchanges.select(&:active).map do |exchange|
-      btcusd = exchange.markets.internal.trading('btc','usd')
-      usdbtc = exchange.markets.internal.trading('usd','btc')
-      [ exchange.name,
-        btcusd.first.depth_runs.where("created_at > ?", time).
-          map{|dr| o=dr.offers.order('price asc').last; [o.created_at.to_i*1000, o.price.to_f] if o},
-        usdbtc.first.depth_runs.where("created_at > ?", time).
-          map{|dr| o=dr.offers.order('price desc').last; [o.created_at.to_i*1000, o.price.to_f] if o},
-      ]
+    @chart_data = exchanges.map do |exchange|
+      data = [ exchange.name, [], [] ]
+      er_shots = snapshots.map do |snapshot|
+        er = snapshot.exchange_runs.find_by_exchange_id(exchange.id)
+        if er
+          er.depth_runs
+        else
+          []
+        end
+      end
+      er_shots.map do |drs|
+        if drs.size > 0
+          data[1] += drs.map{|ss| o=drs.first.offers.order('price asc').last;
+                               [o.created_at.to_i*1000, o.price.to_f] if o}
+          data[2] += drs.map{|ss| o=drs.last.offers.order('price desc').last;
+                             [o.created_at.to_i*1000, o.price.to_f] if o}
+
+        end
+      end
+      data
     end
 
-    strategies = Strategy.where(["created_at > ?", time])
-    @strategy_data = strategies.map{|s| [s.created_at.to_i*1000, s.potential.amount.to_f] if s.potential}
-    @strategy_ids = strategies.map{|s| s.id}
-    @strategy_percentages = strategies.map{|s| [s.created_at.to_i*1000, s.profit_percentage.to_f]}
+    @strategy_data = snapshots.map{|s| [s.strategy.created_at.to_i*1000,
+                                        s.strategy.potential.amount.to_f] if s.strategy}
+    @strategy_ids = snapshots.map{|s| s.strategy.id if s.strategy}
+    @strategy_percentages = snapshots.map{|s| [s.strategy.created_at.to_i*1000,
+                                                s.strategy.profit_percentage.to_f] if s.strategy}
 
     if params[:strategy_id]
       @strategy = Strategy.find(params[:strategy_id])
     else
-      @strategy = Strategy.last
+      @strategy = snapshot.strategy
     end
 
     respond_to do |format|
