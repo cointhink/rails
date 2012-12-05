efe6b0c6783745480e8243fa34548511ca5ab7aa
Don Park <don@donpark.org>
2012-12-05 15:45:19 -0800

    the rate is usd is given as usd

--- test/unit/offer_test.rb
+++ test/unit/offer_test.rb
@@ -11,14 +11,14 @@ class OfferTest < ActiveSupport::TestCase
     offer = setup_valid_offer(price: 12.5, quantity: 3.1)
     rate = offer.rate('usd')
     assert_equal 12.5, rate.amount
-    assert_equal 'btc', rate.currency
+    assert_equal 'usd', rate.currency
   end
 
   def setup_valid_offer(params = {})
     market = Market.create(from_currency: 'usd', to_currency: 'btc',
                            fee_percentage: 0.5)
     depth_run = market.depth_runs.create
-    params = {price: 12.5, quantity: 3.1, market: market}.merge(params)
+    params = {price: 12.5, quantity: 3.1, market: market, currency: 'usd'}.merge(params)
 
     depth_run.offers.create(params)
   end
