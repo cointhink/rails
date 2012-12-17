cf03a62d0df6855fcfcde84208e95b3029c1c947
Don Park <don@donpark.org>
2012-12-17 12:07:04 -0800

    do the migration right

--- db/migrate/20121217182537_add_best_price_to_depth_runs.rb
+++ db/migrate/20121217182537_add_best_price_to_depth_runs.rb
@@ -1,5 +1,20 @@
 class AddBestPriceToDepthRuns < ActiveRecord::Migration
-  def change
+  def up
     add_column :depth_runs, :best_offer_id, :integer
+    puts "backfilling best_offer_id"
+    DepthRun.where("best_offer_id is null").each do |d|
+      if d.market.from_currency == 'usd'
+        best_offer = d.offers.order('price desc').last 
+      else
+        best_offer = d.offers.order('price asc').last 
+      end
+      if best_offer
+        d.update_attribute :best_offer_id, best_offer.id
+      end
+    end
+  end
+
+  def down
+    remove_column :depth_runs, :best_offer_id
   end
 end
