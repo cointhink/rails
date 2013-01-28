a0335c63e0569e6caf229933edc3b55272be27a5
Don Park <don@donpark.org>
2013-01-27 16:17:48 -0800

    front page test, also json

--- test/functional/dash_controller_test.rb
+++ test/functional/dash_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class DashControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "html" do
+    get :chart
+    assert_response :success
+  end
+
+  test "json" do
+    get :chart, :format => :json
+    assert_response :success
+    response_hash = JSON.parse(@response.body)
+  end
 end
