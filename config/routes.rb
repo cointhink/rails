62d8490cec142ee708ae2120fb3da0b9d7f8e6c7
Don Park <don@donpark.org>
2013-04-01 10:40:50 -0700

    /markets/btcusd

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Btcmarketwatch::Application.routes.draw do
   root :to => 'dash#chart'
   resources :exchanges
   resources :strategies
+  match '/markets/btcusd' => 'dash#chart'
   match '/pairs' => 'dash#pairs'
   match '*nowhere' => 'dash#fourohfour'
 end
