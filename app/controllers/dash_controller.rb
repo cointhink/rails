1bf107e4fe350b22e25f22288410d2877b79ebb0
Don Park <don@donpark.org>
2013-04-16 14:10:04 -0700

    fragment cache try 1

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -4,55 +4,30 @@ class DashController < ApplicationController
     start = params[:start] ? Time.parse(params[:start]) : Time.now - @hours.hours
     stop = start + @hours.hours
 
-    if stale?(Snapshot.latest)
-      snapshots = Snapshot.includes(:exchange_runs => [:exchange, :depth_runs]).where(
-                      ['snapshots.created_at > ? and snapshots.created_at < ?', start, stop])
-                   .order('created_at desc')
+    @snapshots = Snapshot.includes(:exchange_runs => [:exchange, :depth_runs]).where(
+                    ['snapshots.created_at > ? and snapshots.created_at < ?', start, stop])
+                 .order('created_at desc')
 
-      chart_data = {}
-      snapshots.each do |snapshot|
-        snapshot.exchange_runs.each do |ex_run|
-          chart_data[ex_run.exchange] ||= [ ex_run.exchange.name, [], [] ]
 
-          depth_runs = ex_run.depth_runs.includes(:best_offer).all
-          if depth_runs.size == 2
-            o=depth_runs.first # tofix: bid/ask detection
-            op = o.best_offer ? o.best_offer.price : nil
-            chart_data[ex_run.exchange][1] << [o.created_at.to_i*1000, op]
-            o=depth_runs.last # tofix: bid/ask detection
-            op = o.best_offer ? o.best_offer.price : nil
-            chart_data[ex_run.exchange][2] << [o.created_at.to_i*1000, op]
-          end
-        end
-      end
-      @chart_data = chart_data.map{|k,v| v}
+    if params[:strategy_id]
+      @strategy = Strategy.find(params[:strategy_id])
+    else
+      @strategy = @snapshots.first.strategy if @snapshots.size > 0
+    end
 
-      @strategy_data = snapshots.map{|s| [s.created_at.to_i*1000,
-                                          s.strategy.potential.amount.to_f] if s.strategy}
-      @strategy_ids = snapshots.map{|s| s.strategy.id if s.strategy}
-      @strategy_percentages = snapshots.map{|s| [s.created_at.to_i*1000,
-                                                  s.strategy.profit_percentage.to_f] if s.strategy}
-
-      if params[:strategy_id]
-        @strategy = Strategy.find(params[:strategy_id])
-      else
-        @strategy = snapshots.first.strategy if snapshots.size > 0
-      end
-
-      respond_to do |format|
-        format.html # index.html.erb
-        format.json do
-          if @strategy
-           render :json => {balance_in: {amount:@strategy.balance_in.amount.round(2),
-                                         currency:@strategy.balance_in.currency},
-                            potential: {amount:@strategy.potential.amount.round(2),
-                                        currency:@strategy.potential.currency},
-                            profit_percentage: @strategy.profit_percentage.round(1),
-                            created_at: @strategy.created_at,
-                            strategy_id: @strategy.id}
-          else
-           render :json => {}
-          end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json do
+        if @strategy
+         render :json => {balance_in: {amount:@strategy.balance_in.amount.round(2),
+                                       currency:@strategy.balance_in.currency},
+                          potential: {amount:@strategy.potential.amount.round(2),
+                                      currency:@strategy.potential.currency},
+                          profit_percentage: @strategy.profit_percentage.round(1),
+                          created_at: @strategy.created_at,
+                          strategy_id: @strategy.id}
+        else
+         render :json => {}
         end
       end
     end
