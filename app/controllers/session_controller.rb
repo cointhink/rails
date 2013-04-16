184d007180ff98faae7e55e7bfc785475c894bef
Don Park <don@donpark.org>
2013-04-16 10:20:04 -0700

    login or sign up

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,10 @@
 class SessionController < ApplicationController
+  def lookup
+    user = User.where(email:params[:email]).first
+    if user
+      redirect_to :login, {:email => params[:email]}
+    else
+      redirect_to :signup
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Btcmarketwatch::Application.routes.draw do
   resources :strategies
   match '/markets/:pair' => 'dash#slider'
   match '/pairs' => 'dash#pairs'
+  match '/session/lookup' => 'session#lookup'
+  match '/session/login' => 'session#login'
+  match '/session/signup' => 'session#signup'
   match '*nowhere' => 'dash#fourohfour'
 end
