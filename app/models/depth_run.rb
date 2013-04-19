b82ccb22a57d09da1746d1bb9e0c52fdab2d9808
Don Park <don@donpark.org>
2013-04-19 12:58:58 -0700

    no room left, time to clear some data

--- app/models/depth_run.rb
+++ app/models/depth_run.rb
@@ -2,7 +2,7 @@ class DepthRun < ActiveRecord::Base
   belongs_to :market
   belongs_to :exchange_run
   belongs_to :best_offer, :class_name => "Offer"
-  has_many :offers
+  has_many :offers, :dependent => :destroy
 
   attr_accessible :exchange_run, :best_price
 
