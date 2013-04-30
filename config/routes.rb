3a9a56618f60f53b8415f231a2055b652a82da12
Don Park <don@donpark.org>
2013-04-30 13:40:32 -0700

    landing page is a menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Btcmarketwatch::Application.routes.draw do
-  root :to => 'dash#chart'
+  root :to => 'dash#menu'
   resources :exchanges
   resources :strategies
   resources :users
+
+  match '/arbitrage/:pair' => 'dash#chart'
+  match '/arbitrage' => 'dash#chart'
   match '/markets/:pair' => 'dash#slider'
   match '/pairs' => 'dash#pairs'
+
   post '/session/create' => 'session#create'
   match '/session/lookup' => 'session#lookup'
   match '/session/login' => 'session#login'
   match '/session/logout' => 'session#logout'
   match '/session/signup' => 'session#signup'
+
   match '*nowhere' => 'dash#fourohfour'
 end
