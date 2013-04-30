3af73879cdad78d666a6474888dddb9f4ac761a6
Don Park <don@donpark.org>
2013-04-30 14:15:36 -0700

    its okay to leave out the listed_at date

--- app/models/markets/campbx.rb
+++ app/models/markets/campbx.rb
@@ -12,7 +12,6 @@ class Markets::Campbx
     end
     data[offer_type+"s"].map do |offer|
       { bidask: offer_type,
-        listed_at: Time.now, #cbx doesn't provide this
         price: offer[0],
         quantity: offer[1],
         currency: currency
