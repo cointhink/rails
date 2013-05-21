26fd66328a046b2cd2dfaae1d7b919e921aac011
Don Park <don@donpark.org>
2013-05-21 10:44:33 -0700

    no cointainer handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @balances = COIND.map do |name, coind|
+      coin_record = {"name" => name}
+      begin
       info = coind.balance(@user.username)
-      {"name" => name, "balance" => info["balance"]}
+      coin_record["balance"] = info["balance"]
+      rescue Errno::ECONNREFUSED
+        coin_record["error"] = "not available"
+      end
+      coin_record
     end
   end
 end
