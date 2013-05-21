5463064e8a8e692d4858e1c5e1318c73534ab7b1
Don Park <don@donpark.org>
2013-05-21 09:51:59 -0700

    display something even on an empty database

--- app/controllers/dash_controller.rb
+++ app/controllers/dash_controller.rb
@@ -46,20 +46,23 @@ class DashController < ApplicationController
   end
 
   def menu
-    @snapshot = Snapshot.last
-    latest_exchanges = @snapshot.exchange_runs.map{|er| er.exchange}
-    @exchanges = Exchange.with_markets('btc','usd').map do |m|
-      e = {:exchange => m[:exchange], :cost => 0} #descriptive data
-      if latest_exchanges.include?(m[:exchange])
-        exchange_runs = @snapshot.exchange_runs.select{|er| er.exchange == m[:exchange]}.first
-        e[:depth_runs] = exchange_runs.depth_runs.reduce({}){|m,dr| m[dr.market.bidask('usd')]=dr.cost;m}
-        e[:cost] = e[:depth_runs].values.sum{|cost| cost.amount.to_f}
+    @snapshot = Snapshot.last || Snapshot.new
+    @exchanges = []
+    if @snapshot
+      latest_exchanges = @snapshot.exchange_runs.map{|er| er.exchange}
+      @exchanges = Exchange.with_markets('btc','usd').map do |m|
+        e = {:exchange => m[:exchange], :cost => 0} #descriptive data
+        if latest_exchanges.include?(m[:exchange])
+          exchange_runs = @snapshot.exchange_runs.select{|er| er.exchange == m[:exchange]}.first
+          e[:depth_runs] = exchange_runs.depth_runs.reduce({}){|m,dr| m[dr.market.bidask('usd')]=dr.cost;m}
+          e[:cost] = e[:depth_runs].values.sum{|cost| cost.amount.to_f}
+        end
+        e
       end
-      e
+      logger.info @exchanges.map{|e| [e[:name], e[:cost]]}
+      @exchanges.sort_by!{|e| e[:cost]}.reverse!
+      logger.info @exchanges.map{|e| [e[:name], e[:cost]]}
     end
-    logger.info @exchanges.map{|e| [e[:name], e[:cost]]}
-    @exchanges.sort_by!{|e| e[:cost]}.reverse!
-    logger.info @exchanges.map{|e| [e[:name], e[:cost]]}
   end
 
   def fourohfour
