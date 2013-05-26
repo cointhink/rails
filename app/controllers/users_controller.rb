97dd66ba891cab099ccbd62e8a323b9caefc7d74
Don Park <donp@donp.org>
2013-05-26 10:18:55 -0700

    cointainer user call

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @balances = COIND.map do |name, coind|
-      coin_record = {"name" => name}
       begin
-      info = coind.balance(@user.username)
-      coin_record["balance"] = info["balance"]
+        coind.user(@user.username).merge({"name"=>name})
       rescue Errno::ECONNREFUSED
-        coin_record["error"] = "not available"
+        {"error" => "not available"}
       end
-      coin_record
     end
   end
 end
