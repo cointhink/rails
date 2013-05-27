375e185dcc35ec1b9c1fd3f5240be70d48f5bdb0
Don Park <donp@donp.org>
2013-05-27 11:44:14 -0700

    speed up offer deletes

--- app/models/depth_run.rb
+++ app/models/depth_run.rb
@@ -3,7 +3,7 @@ class DepthRun < ActiveRecord::Base
   belongs_to :exchange_run
   belongs_to :best_offer, :class_name => "Offer"
   belongs_to :cost, :class_name=>"Balance"
-  has_many :offers, :dependent => :destroy
+  has_many :offers, :dependent => :delete_all
 
   attr_accessible :exchange_run, :best_offer, :cost
 
