2b24d7e2c389b7b4943dd596abd3ff56fdc628a6
Don Park <donp@donp.org>
2013-06-10 12:00:08 -0700

    monkeypatch a backdoor

--- test/functional/scripts_controller_test.rb
+++ test/functional/scripts_controller_test.rb
@@ -3,15 +3,21 @@ require 'test_helper'
 class ScriptsControllerTest < ActionController::TestCase
 
   test "start" do
-    login
+    login(User.new)
     post :start, {scriptname:"average", username:"jill"}
     assert_nil flash[:error]
     assert_redirected_to :action => :lastrun
   end
 
-  def login
+  def login(user)
+    # setup a backdoor
+    def @controller.mock_user(user)
+      @mock_user = user
+    end
+    @controller.mock_user(user)
+    # use the backdoor
     def @controller.auth
-      log_in(User.new)
+      log_in(@mock_user)
     end
   end
 end
