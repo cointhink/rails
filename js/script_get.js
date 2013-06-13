b1bf8159052cde1adcf659c296fe72d99897eb20
Don Park <donp@donp.org>
2013-06-13 16:27:58 -0700

    this is getting ugly

--- js/script_get.js
+++ js/script_get.js
@@ -2,6 +2,7 @@ var bouncy = require('bouncy');
 var r = require('rethinkdb');
 var fs = require('fs');
 var path = require('path');
+var iso8601 = require('iso8601');
 
 var server = bouncy(function (req, res, bounce) {
   var parts = req.url.split('/');
@@ -15,14 +16,34 @@ var server = bouncy(function (req, res, bounce) {
     res.statusCode = 200;
     res.end(fs.readFileSync('npm/'+path.basename(scriptname)));
   } else {
-   console.log('fetching '+username+'/'+scriptname)
+    var fullname = username+'/'+scriptname
+    console.log('fetching '+fullname)
     r.connect({host:'localhost', port:28015, db:'cointhink'},
       function(err, conn) {
-        r.table('scripts').get(username+'/'+scriptname).run(conn, function(err, doc){
+        r.table('scripts').get(fullname).run(conn, function(err, doc){
           if(doc) {
             if(doc.key == key) {
               res.statusCode = 200;
-              res.end(doc.body);
+              // Authorized
+              if(req.method == 'GET') {
+                res.end(doc.body);
+              }
+              if(req.method == 'POST') {
+                req.on("data",function(data){
+                  var body = data.toString('utf8')
+                  sig_doc = JSON.parse(body)
+                  sig_doc.name = username+'/'+scriptname
+                  sig_doc.time = new Date() //iso8601.fromDate(new Date())
+                  console.log('posting '+JSON.stringify(sig_doc))
+                  r.table('signals').insert(sig_doc).run(conn, function(err, doc){
+                    if(err){
+                      res.end({error: err});
+                    } else {
+                      res.end(JSON.stringify(doc));
+                    }
+                  })
+                })
+              }
             } else {
               res.statusCode = 403;
               res.end(JSON.stringify({error: "bad key"}));
