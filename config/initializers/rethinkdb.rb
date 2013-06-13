23d6a9a10e39f4d66ef0f365f7c8d9bd19bd9fdb
Don Park <donp@donp.org>
2013-06-13 11:03:40 -0700

    string/hash craziness

--- config/initializers/rethinkdb.rb
+++ config/initializers/rethinkdb.rb
@@ -1,5 +1,7 @@
 include RethinkDB::Shortcuts
-R = r.connect(SETTINGS["rethinkdb"])
+R = r.connect(:host => SETTINGS["rethinkdb"]["host"],
+              :port => SETTINGS["rethinkdb"]["port"],
+              :db => SETTINGS["rethinkdb"]["db"])
 unless r.db_list.run(R).include?(SETTINGS["rethinkdb"]["db"])
   puts "rethinkdb: creating database '#{SETTINGS["rethinkdb"]["db"]}'"
   r.db_create(SETTINGS["rethinkdb"]["db"]).run(R)
