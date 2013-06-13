605dc2d45b1d70c071badbe1e1a23a8aa8888b83
Don Park <donp@donp.org>
2013-06-13 14:34:47 -0700

    serve local files for npm distribution

--- js/script_get.js
+++ js/script_get.js
@@ -1,5 +1,7 @@
 var bouncy = require('bouncy');
 var r = require('rethinkdb');
+var fs = require('fs');
+var path = require('path');
 
 var server = bouncy(function (req, res, bounce) {
   var parts = req.url.split('/');
@@ -8,25 +10,32 @@ var server = bouncy(function (req, res, bounce) {
   var key = parts[3]
 
   console.log(req.method+" "+req.url)
-  console.log('fetching '+username+'/'+scriptname)
-  r.connect({host:'localhost', port:28015, db:'cointhink'},
-    function(err, conn) {
-      r.table('scripts').get(username+'/'+scriptname).run(conn, function(err, doc){
-        if(doc) {
-          if(doc.key == key) {
-            res.statusCode = 200;
-            res.end(doc.body);
+ if(username == 'npm' && key == null) {
+    console.log('fetching npm '+scriptname)
+    res.statusCode = 200;
+    res.end(fs.readFileSync('npm/'+path.basename(scriptname)));
+  } else {
+   console.log('fetching '+username+'/'+scriptname)
+    r.connect({host:'localhost', port:28015, db:'cointhink'},
+      function(err, conn) {
+        r.table('scripts').get(username+'/'+scriptname).run(conn, function(err, doc){
+          if(doc) {
+            if(doc.key == key) {
+              res.statusCode = 200;
+              res.end(doc.body);
+            } else {
+              res.statusCode = 403;
+              res.end(JSON.stringify({error: "bad key"}));
+
+            }
           } else {
             res.statusCode = 403;
-            res.end(JSON.stringify({error: "bad key"}));
-
+            res.end(JSON.stringify({error: "not found"}));
           }
-        } else {
-          res.statusCode = 403;
-          res.end(JSON.stringify({error: "not found"}));
-        }
-      })
-    })
+        })
+      }
+    )
+  }
 });
 console.log("listening on 3002")
 server.listen(3002);
