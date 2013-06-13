fda4a4d7f2e666434228368825392baeddbe3275
Don Park <donp@donp.org>
2013-06-13 10:56:36 -0700

    rethinkdb 1.6

--- config/initializers/rethinkdb.rb
+++ config/initializers/rethinkdb.rb
@@ -1,7 +1,8 @@
 include RethinkDB::Shortcuts
-R = r.connect(SETTINGS["rethinkdb"]["host"],SETTINGS["rethinkdb"]["port"],SETTINGS["rethinkdb"]["db"])
+R = r.connect(SETTINGS["rethinkdb"])
 unless r.db_list.run(R).include?(SETTINGS["rethinkdb"]["db"])
   puts "rethinkdb: creating database '#{SETTINGS["rethinkdb"]["db"]}'"
   r.db_create(SETTINGS["rethinkdb"]["db"]).run(R)
   r.table_create('scripts').run(R)
+  r.table_create('signals').run(R)
 end
