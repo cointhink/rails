4470954602020841e0c9ba5382b51161ed6bea8f
Don Park <donp@donp.org>
2013-06-13 17:08:07 -0700

    secondary index for signals

--- config/initializers/rethinkdb.rb
+++ config/initializers/rethinkdb.rb
@@ -7,4 +7,5 @@ unless r.db_list.run(R).include?(SETTINGS["rethinkdb"]["db"])
   r.db_create(SETTINGS["rethinkdb"]["db"]).run(R)
   r.table_create('scripts').run(R)
   r.table_create('signals').run(R)
+  r.table('signals').index_create('name').run(R)
 end
