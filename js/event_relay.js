bc722f4c6af06fe42c772a5a2097fd71da0c25fe
Don Park <donp@donp.org>
2013-06-21 14:38:56 -0700

    zmq does not use dns

--- js/event_relay.js
+++ js/event_relay.js
@@ -3,7 +3,7 @@ var zmq = require('zmq'),
 var socketio = require('socket.io-client'),
     mtgox    = require('mtgox-orderbook')
 
-sock.bindSync('tcp://dockerhost:3001');
+sock.bindSync('tcp://172.16.42.1:3001');
 console.log('zmq push bound to port 3001');
 
 var sockio = socketio.connect(mtgox.socketio_url)
