f24dcd43a37bff25b077a80906c1a02d240579c1
Don Park <donp@donp.org>
2013-06-23 12:10:51 -0700

    report connect date

--- js/event_relay.js
+++ js/event_relay.js
@@ -4,11 +4,11 @@ var socketio = require('socket.io-client'),
     mtgox    = require('mtgox-orderbook')
 
 sock.bindSync('tcp://172.16.42.1:3001');
-console.log('zmq push bound to port 3001');
+console.log('event_relay zmq push bound to port 3001');
 
 var sockio = socketio.connect(mtgox.socketio_url)
 var obook = mtgox.attach(sockio, 'usd')
-console.log('connecting to mtgox')
+console.log((new Date())+' connecting to mtgox')
 
 obook.on('ticker', function(ticker){
   var data = JSON.stringify(ticker)
