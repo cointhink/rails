a8abf4300fc71f7e9f101527348ef64fbbbfad56
Don Park <donp@donp.org>
2013-07-02 14:57:04 -0700

    cointhink.trade

--- js/storage_relay.js
+++ js/storage_relay.js
@@ -43,6 +43,23 @@ sock.on('message', function(data){
                     respond({"status":"ok", "payload":status})
                   })
                 }
+                if(payload.action == 'trade'){
+                  console.log(fullname+' trade '+payload.exchange+' '+payload.market+' '+payload.buysell)
+                  var response = trade(payload, doc.inventory)
+                  if(response.status == 'ok'){
+                    r.table('scripts').get(fullname)('trades').
+                    prepend(response.payload.trade).run(conn, function(err){
+                      if(err) console.log(err)
+                      r.table('scripts').get(fullname).
+                      update({inventory:doc.inventory}).run(conn, function(err){
+                        if(err) console.log(err)
+                        respond(response)
+                      })
+                    })
+                  } else {
+                    respond(response)
+                  }
+                }
               } else {
                 console.log(fullname+" bad key!")
                 respond({"status":"badkey"})
@@ -63,4 +80,38 @@ sock.on('message', function(data){
 
 function respond(payload){
   sock.send(JSON.stringify(payload))
+}
+
+function trade(payload, inventory){
+//trade('mtgox','btc',4,'buy','usd',92)
+//trade('mtgox','btc',4,'sell','usd',97)
+  var result
+  if(payload.buysell == 'buy') {
+    var on_hand = inventory[payload.currency]
+    if(on_hand){
+      var price = (payload.amount * payload.quantity)
+      if(on_hand >= price) {
+        inventory[payload.currency] -= price
+        result = {"status":"ok", payload: {trade:payload, inventory:inventory}}
+      } else {
+        result = {"status":"err", payload:""+on_hand+payload.currency+" is in sufficient for "+price}
+      }
+    } else {
+      result = {"status":"err", payload:"no "+payload.currency+" in inventory"}
+    }
+  } else if (payload.buysell == 'sell') {
+    var on_hand = inventory[payload.market]
+    if(on_hand){
+      if(on_hand >= payload.quantity){
+        inventory[payload.market] -= payload.quantity
+        result = {"status":"ok", payload: {trade:payload, inventory:inventory}}
+      } else {
+        result = {"status":"err", payload:""+on_hand+payload.market+" is in sufficient for "+payload.quantity}
+      }
+    } else {
+      result = {"status":"err", payload:"no "+payload.market+" in inventory"}
+    }
+  }
+
+  return result
 }
