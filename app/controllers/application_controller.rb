bbda477c586dc9e48792649ab73e25ddf4857403
Don Park <donp@donp.org>
2013-07-02 08:39:31 -0700

    handle crazy accept: types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
   include Routy
 
   before_filter :auth
+
+  # a valid route, with an invalid accepts header (image/*) causes this
+  rescue_from  ActionView::MissingTemplate, :with => :missing_template
+
+  def missing_template
+    render :nothing => true, :status => 406
+  end
 end
