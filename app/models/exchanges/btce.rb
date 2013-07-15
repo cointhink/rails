114de19985f49c3eced2cd32be23c197b3b52b77
Don Park <donp@donp.org>
2013-07-15 11:00:09 -0700

    currency support for btce

--- app/models/exchanges/btce.rb
+++ app/models/exchanges/btce.rb
@@ -2,7 +2,7 @@ class Exchanges::Btce < Exchanges::Base
   def depth_poll(conn, from_currency, to_currency)
     # public api: /ticker /trades /depth
     # covers two markets, from/to and to/from
-    url = 'https://btc-e.com/api/2/btc_usd/depth'
+    url = "https://btc-e.com/api/2/#{from_currency}_#{to_currency}/depth"
     JSON.parse(conn.get(url).body)
   end
 end
