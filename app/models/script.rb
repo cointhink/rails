180e6848382e678e90cb5453a4fca830af72946e
Don Park <donp@donp.org>
2013-07-25 10:37:24 -0700

    hotfix

--- app/models/script.rb
+++ app/models/script.rb
@@ -70,7 +70,7 @@ class Script < ActiveRecord::Base
   end
 
   def reset!
-    r.table('storage').get(script_name).update(storage:{}).run(R)
+    r.table('storage').get(script_name).update({}).run(R)
   end
 
   def script_name
