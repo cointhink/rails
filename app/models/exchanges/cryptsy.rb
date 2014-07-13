1f1cbcaec7edb8661a1d78e5ac5c8d9b209242a7
Don Park <donp@donp.org>
2014-07-13 12:46:16 -0700

    cryptsy api url change

--- app/models/exchanges/cryptsy.rb
+++ app/models/exchanges/cryptsy.rb
@@ -2,7 +2,16 @@ class Exchanges::Cryptsy < Exchanges::Base
   def depth_poll(conn, from_currency, to_currency)
     # public api: /ticker /trades /depth
     # covers two markets, from/to and to/from
-    url = "http://pubapi.cryptsy.com/api.php?method=orderdata"
+    if from_currency == "usd" && to_currency == "btc"
+        market_id = "2"
+    end
+    if from_currency == "ltc" && to_currency == "btc"
+        market_id = "3"
+    end
+    if from_currency == "doge" && to_currency == "btc"
+        market_id = "132"
+    end
+    url = "http://pubapi.cryptsy.com/api.php?method=singleorderdata&marketid=#{market_id}"
     orderbook = JSON.parse(conn.get(url).body)["return"]
     market = orderbook[from_currency.upcase]
     asks = market["sellorders"].map{|o| [o["price"],o["quantity"]]}
