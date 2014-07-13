609148262fb0a8a939992e86713cf5913e971060
Don Park <donp@donp.org>
2014-07-13 12:30:38 -0700

    slight api url change

--- app/models/exchanges/bitcoin24.rb
+++ app/models/exchanges/bitcoin24.rb
@@ -1,6 +1,6 @@
 class Exchanges::Bitcoin24 < Exchanges::Base
   def ticker_poll(from_currency, to_currency)
-    url = "https://bitcoin-24.com/api/#{to_currency.upcase}/ticker.json"
+    url = "https://bitcoin-24.com/api/ticker.json"
     data = JSON.parse(Faraday.get(url).body)
     attrs = {:highest_bid_usd => data["ask"],
              :lowest_ask_usd => data["bid"]}
@@ -8,7 +8,7 @@ class Exchanges::Bitcoin24 < Exchanges::Base
 
   def depth_poll(conn, from_currency, to_currency)
     # covers two markets, from/to and to/from
-    url = "https://bitcoin-24.com/api/#{to_currency.upcase}/orderbook.json"
+    url = "https://bitcoin-24.com/api/orderbook.json"
     JSON.parse(conn.get(url).body)
   end
 end
