ced7436b8f687be6b99bd0a11fa746de5071d21e
Don Park <donp@donp.org>
2014-12-03 15:26:34 -0800

    cert skip

--- app/models/exchanges/btce.rb
+++ app/models/exchanges/btce.rb
@@ -2,6 +2,7 @@ class Exchanges::Btce < Exchanges::Base
   def depth_poll(conn, from_currency, to_currency)
     # public api: /ticker /trades /depth
     # covers two markets, from/to and to/from
+    OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
     url = "https://btc-e.com/api/2/#{from_currency}_#{to_currency}/depth"
     JSON.parse(conn.get(url).body)
   end
