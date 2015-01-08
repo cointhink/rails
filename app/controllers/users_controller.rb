d0a51cf2ef7a9914caec6fbd454c846ecd465301
Don Park <donp@donp.org>
2015-01-08 11:08:15 -0800

    find_by_slug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,37 +2,41 @@ class UsersController < ApplicationController
   before_filter :require_login, :except => [:show]
 
   def show
-    @user = User.find(params[:id])
-    if logged_in? &&
-       current_user == @user
-      @balances = {}
-      COIND.each do |coinname, coind|
-        begin
+    @user = User.find_by_slug(params[:id])
+    if @user
+      if logged_in? && current_user == @user
+        @balances = {}
+        COIND.each do |coinname, coind|
           begin
-          @balances[coinname] = coind.user(current_user.username)
-          rescue Jimson::Client::Error::ServerError => e
-            logger.info e
-            @balances[coinname] = {"error" => "unavailable"}
+            begin
+            @balances[coinname] = coind.user(current_user.username)
+            rescue Jimson::Client::Error::ServerError => e
+              logger.info e
+              @balances[coinname] = {"error" => "unavailable"}
+            end
+          rescue Errno::ECONNREFUSED
+            @balances[coinname] = {"error" => "not enabled"}
           end
-        rescue Errno::ECONNREFUSED
-          @balances[coinname] = {"error" => "not enabled"}
         end
-      end
 
-      @transactions = []
-      COIND.each do |coinname, coind|
-        begin
-          tx_resp = coind.transactions(current_user.username)
-          if tx_resp["transactions"]
-            tx = tx_resp["transactions"].map{|t|
-                       t.merge({'currency_code' => SETTINGS["cryptocoins"][coinname]["currency_code"]})}
-            @transactions << tx
+        @transactions = []
+        COIND.each do |coinname, coind|
+          begin
+            tx_resp = coind.transactions(current_user.username)
+            if tx_resp["transactions"]
+              tx = tx_resp["transactions"].map{|t|
+                         t.merge({'currency_code' => SETTINGS["cryptocoins"][coinname]["currency_code"]})}
+              @transactions << tx
+            end
+          rescue Errno::ECONNREFUSED, Jimson::Client::Error::ServerError => e
+            logger.info e
           end
-        rescue Errno::ECONNREFUSED, Jimson::Client::Error::ServerError => e
-          logger.info e
         end
+        @transactions.flatten!
       end
-      @transactions.flatten!
+    else
+      flash[:error] = "User \"#{params[:id]}\" does not exist."
+      redirect_to '/'
     end
   end
 
